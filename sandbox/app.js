5469de0ce5d1a9bc8fd47d4d5df78be8aa50df99
leandrofahur <leandrofahur@gmail.com>
2020-07-03 23:53:16 -0700

    needs refactorization

--- sandbox/app.js
+++ sandbox/app.js
@@ -3,10 +3,35 @@ const canvas = document.getElementById('gameCanvas');
 const canvasContext = canvas.getContext('2d');
 const frameRate = 30;
 
+let mousePosX = 0;
+let mousePosY = 0;
+
 window.onload = () => {
+  gameWorld(); 
+  worldMap();
   setInterval(game, 1000/frameRate);
+  addEventListener('mousemove', onUpdateMousePos);
 }
 
 const game = () => {
-  // console.log('sanity test...');
+  gameWorld();
+  canvasContext.drawImage(img, 0, 0);
+  colorText(mousePosX, mousePosY, `(${mousePosX}, ${mousePosY})`, 'yellow');
+
 }
+
+// ----------------------------------------------------------------------------------------
+// GRAPHICS AND SCENES:
+const gameWorld = () => {
+  baseWorldWithGrid();
+}
+
+// HACK:
+const onUpdateMousePos = (evt) => {
+  let rect = canvas.getBoundingClientRect();
+  let root = document.documentElement;
+
+  mousePosX = Math.floor(evt.clientX - rect.left - root.scrollLeft);
+  mousePosY = Math.floor(evt.clientY - rect.top - root.scrollTop);
+}
+
---
+++ sandbox/js/graphicsCommon.js
@@ -0,0 +1,67 @@
+// GLOBAL VARS:
+const binMap = [1,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0];
+
+const GROUND = 0;
+let img = document.createElement('img');
+let imgLoaded = false;
+
+// ----------------------------------------------------------------------------------------
+// GRAPHICS AND SCENES:
+const colorRect = (posX, posY, width, height, colorNameOrNumber) => {
+  canvasContext.fillStyle = colorNameOrNumber;
+  canvasContext.fillRect(posX, posY, width, height);
+}
+
+const colorLine = (initPosX, initPosY, finalPosX, finalPosY, colorNameOrNumber) => {
+  canvasContext.beginPath();
+  canvasContext.strokeStyle = colorNameOrNumber;
+  canvasContext.moveTo(initPosX, initPosY);
+  canvasContext.lineTo(finalPosX, finalPosY);
+  canvasContext.stroke();
+}
+
+const colorText = (posX, posY, words2Display, colorNameOrNumber) => {
+  canvasContext.fillStyle = colorNameOrNumber;
+  canvasContext.fillText(words2Display, posX, posY, )
+}
+
+const grid = (stepX, stepY) => {
+  for(let col = 0; col <= canvas.width; col += stepX) {
+    colorLine(col, 0, col, canvas.width, 'white');
+  }
+  for(let row = 0; row <= canvas.height; row += stepY) {
+    colorLine(0, row, canvas.width, row, 'white');
+  }
+}
+
+const baseWorldWithGrid = () => {
+  colorRect(0, 0, canvas.width, canvas.height, 'black');
+  grid(canvas.width/10, canvas.height/10);
+  // colorText(mousePosX, mousePosY, `(${mousePosX}, ${mousePosY})`, 'yellow');
+}
+
+const loadImages = (imgNumber) => { 
+  img.onload = () => {
+    imgLoaded = true;
+  }
+  img.src = './assets/scenario/ground.png';
+}
+
+const worldMap = () => {
+  binMap.forEach((index) => {
+    if(index == 0) {
+      loadImages();
+    }
+  });
+};
+
+
