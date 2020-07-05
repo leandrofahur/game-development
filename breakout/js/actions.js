309c1da0def665c2a5c70b89fdff1fc4e43cf53d
leandrofahur <leandrofahur@gmail.com>
2020-07-05 02:18:10 -0700

    implementing ball paddle colision

--- breakout/js/actions.js
+++ breakout/js/actions.js
@@ -4,20 +4,20 @@ const draw = () => {
   // Debbugind purpose:
   grid(PADDLE_WIDTH, PADDLE_HEIGHT);
   // Ball:
-  colorCircle(ballPosX, ballPosY, BALL_RADIUS, 'white');
+  colorCircle(ballPosX, ballPosY, BALL_RADIUS, 'grey');
   // Paddle:
-  colorBlock(paddlePosX - PADDLE_WIDTH/2, paddlePosY + PADDLE_OFFSET, PADDLE_WIDTH, PADDLE_HEIGHT, 'white');
+  colorBlock(paddlePosX, paddlePosY + PADDLE_OFFSET, PADDLE_WIDTH, PADDLE_HEIGHT, 'white');
 }
 
 const move = () => {
   ballColision();
   paddleColision();
+  ballPaddleColision();
 }
 
 const ballColision = () => {
   // ballPosX += ballVelX;
   // ballPosY += ballVelY;
-
   if(ballPosX - BALL_RADIUS < 0) {
     ballVelX *= -1;
   }
@@ -34,7 +34,6 @@ const ballColision = () => {
 
 const paddleColision = () => {
   // paddlePosX += paddleVelX;
-  
   if(paddlePosX - PADDLE_WIDTH/2 < 0) {
     paddleVelX *= -1;
   }
@@ -43,9 +42,22 @@ const paddleColision = () => {
   }
 }
 
+const ballPaddleColision = () => {
+  ballPosY += ballVelY;
+  // paddlePosX += paddleVelX;
+  if(ballPosY + BALL_RADIUS > paddlePosY + PADDLE_OFFSET
+     && ballPosX >= paddlePosX && ballPosX <= paddlePosX + PADDLE_WIDTH) {
+    ballVelY *= -1;
+  }
+}
+
+const showMousePos = () => {
+  colorText(`(${mousePosX}, ${mousePosY})`, mousePosX, mousePosY, 'yellow');
+}
+
 const game = () => {
   draw();
   move();  
-  colorText(`(${mousePosX}, ${mousePosY})`, mousePosX, mousePosY, 'yellow');
-  console.log(`${paddlePosX}, ${paddlePosY}`);
+  // showMousePos();
+  // console.log(`${paddlePosX}, ${paddlePosY}`);
 }
